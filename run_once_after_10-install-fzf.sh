56ed3ad5100913745e8b0928a99eed9df7128030
Ryo Takahashi <reiyw.setuve@gmail.com>
2023-04-08 01:18:00 +0900

    Install fzf manually

--- run_once_after_10-install-fzf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [[ -e "$HOME/.fzf.zsh" ]]; then
-    echo "fzf already installed"
-else
-    ~/.fzf/install --key-bindings --completion --no-update-rc --no-bash --no-fish
-fi
