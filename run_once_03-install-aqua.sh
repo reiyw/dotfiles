6d07f5666104406f953fd3d406d15c00111cd65e
Ryo Takahashi <reiyw.setuve@gmail.com>
2023-09-11 03:05:39 +0900

    Manage some packages in aqua

---
+++ run_once_03-install-aqua.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v2.1.1/aqua-installer
+echo "c2af02bdd15da6794f9c98db40332c804224930212f553a805425441f8331665  aqua-installer" | sha256sum -c
+chmod +x aqua-installer
+./aqua-installer
+rm aqua-installer
