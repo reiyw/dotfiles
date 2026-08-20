25a628df1b601848756e2dfbfdfced14fa914379
Ryo Takahashi <reiyw.setuve@gmail.com>
2026-08-20 17:13:29 +0900

    Prefer aqua-managed tree-sitter in Neovim: add CLI to aqua and prepend aqua bin to PATH (#1034)

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,9 @@
+-- Prefer tools managed by aqua over binaries supplied by plugin managers.
+local aqua_root = vim.env.AQUA_ROOT_DIR or vim.fn.expand("~/.local/share/aquaproj-aqua")
+local aqua_bin = aqua_root .. "/bin"
+if vim.fn.isdirectory(aqua_bin) == 1 then
+  vim.env.PATH = aqua_bin .. ":" .. vim.env.PATH
+end
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
